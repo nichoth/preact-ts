80f2b3d5b9c8fccbf32ef8120fa67bff1010216e
Nick Thomas <nichoth@gmail.com>
2017-08-30 20:44:34 -0700

    first commit

---
+++ src/Component.jsx
@@ -0,0 +1,9 @@
+import { h, render, Component } from 'preact';
+
+export default class Clock extends Component {
+    render() {
+        let time = new Date().toLocaleTimeString();
+        return <span>{ time }</span>;
+    }
+}
+
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import Clock from './Component.jsx'
+import { h, render } from 'preact'
+
+function log (s: string) {
+    console.log(s)
+}
+
+log('hello')
+
+// render an instance of Clock into <body>:
+render(<Clock />, document.body);
